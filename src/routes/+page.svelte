4ec7077e460af6d6da1c62f64dd9beb767669133
Azdgari <109515968+Azdgari@users.noreply.github.com>
2024-01-15 18:15:50 +0700

    Structure basic HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1 class="text-green-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the tation</p>
+<main>
+  <header>
+    <h1>Where in the world?</h1>
+    <button>Dark Mode</button>
+  </header>
+
+<body-container>
+  <filter-bar>
+    <input type="text" placeholder="Search for a country...">
+    <select placeholder="Filter by Region">
+      <option value="Africa">Africa</option>
+      <option value="America">America</option>
+      <option value="Asia">Asia</option>
+      <option value="Europe">Europe</option>
+      <option value="Oceania">Oceania</option>
+    </select>
+  </filter-bar>
+  <country-container>
+    <country-card>
+      <!-- <img src="https://restcountries.eu/data/afg.svg" alt="Afghan flag"> -->
+      <h2>Afghanistan</h2>
+      <p><strong>Population:</strong> 27657145</p>
+      <p><strong>Region:</strong> Asia</p>
+      <p><strong>Capital:</strong> Kabul</p>
+    </country-card>
+  </country-container>
+</body-container>
+
+</main>
