4cd3d88187e7fcd07372897d20943371dc1eca15
Azdgari <109515968+Azdgari@users.noreply.github.com>
2024-01-16 18:38:50 +0700

    Broke app with fetch function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+    /** @type {import('./$types').PageData} */
+
+  // let data = [];
+
+  // onMount(async () => {
+  //   const response = await fetch('$lib/data.json')
+  //   if (response.ok) {
+  //    data = await response.json()
+  //   } else {
+  //     console.log('Fetch error', response.statusText)
+  //   }
+  //   })
+
+
+</script>
+
 <main>
   <header class="flex justify-between px-14 py-5 bg-dark-mode-elements">
     <h1 class="font-bold text-dark-mode-text">Where in the world?</h1>
@@ -5,9 +23,9 @@
   </header>
 
 <section class="mx-14">
-  <div class="filter flex justify-between my-10">
-    <input class="flex-2 min-w-96 px-8 bg-dark-mode-elements rounded-sm" type="text" placeholder="Search for a country..." />
-    <select class="dropdown py-3 min-w-60 bg-dark-mode-elements rounded-sm" >
+  <div class="filter justify-between my-10 md:flex gap-10">
+    <input class="flex-2 min-w-96 px-8 h-12 bg-dark-mode-elements rounded-sm" type="text" placeholder="Search for a country..." />
+    <select class="dropdown select mt-8 md:mt-0 select-bordered w-72 max-w-xs bg-dark-mode-elements rounded-sm" >
       <option value="" disabled selected>Filter by Region</option>
       <option value="Africa">Africa</option>
       <option value="America">America</option>
@@ -15,52 +33,76 @@
       <option value="Europe">Europe</option>
       <option value="Oceania">Oceania</option>
     </select>
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn m-1">Click</div>
-      <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-      </ul>
-    </div>
+
   </div>
-  <section>
-    <div class='country-card'>
-      <div class='flag'>Flag</div>
-      <div class='country-data'>
-        <h2>Afghanistan</h2>
+    <section class="md:flex flex-wrap gap-20">
+
+    <div class="card w-72 bg-base-100 shadow-xl rounded-md">
+      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <div class="card-body">
+        <h2 class="card-title">Bahamia</h2>
+        <h1>{data.post.title}</h1>
+        <div>{@html data.post.content}</div>
         <p><strong>Population:</strong> 27657145</p>
         <p><strong>Region:</strong> Asia</p>
         <p><strong>Capital:</strong> Kabul</p>
       </div>
     </div>
-    <div class='country-card'>
-      <div class='flag'>Flag</div>
-      <div class='country-data'>
-        <h2>Afghanistan</h2>
+
+    <div class="card w-72 bg-base-100 shadow-xl rounded-md">
+      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <div class="card-body">
+        <h2 class="card-title">Bahamia</h2>
         <p><strong>Population:</strong> 27657145</p>
         <p><strong>Region:</strong> Asia</p>
         <p><strong>Capital:</strong> Kabul</p>
       </div>
     </div>
-    <div class='country-card'>
-      <div class='flag'>Flag</div>
-      <div class='country-data'>
-        <h2>Afghanistan</h2>
+
+    <div class="card w-72 bg-base-100 shadow-xl rounded-md">
+      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <div class="card-body">
+        <h2 class="card-title">Bahamia</h2>
         <p><strong>Population:</strong> 27657145</p>
         <p><strong>Region:</strong> Asia</p>
         <p><strong>Capital:</strong> Kabul</p>
       </div>
     </div>
-    <div class='country-card'>
-      <div class='flag'>Flag</div>
-      <div class='country-data'>
-        <h2>Afghanistan</h2>
+
+    <div class="card w-72 bg-base-100 shadow-xl rounded-md">
+      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <div class="card-body">
+        <h2 class="card-title">Bahamia</h2>
         <p><strong>Population:</strong> 27657145</p>
         <p><strong>Region:</strong> Asia</p>
         <p><strong>Capital:</strong> Kabul</p>
       </div>
     </div>
+
+    <div class="card w-72 bg-base-100 shadow-xl rounded-md">
+      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <div class="card-body">
+        <h2 class="card-title">Bahamia</h2>
+        <p><strong>Population:</strong> 27657145</p>
+        <p><strong>Region:</strong> Asia</p>
+        <p><strong>Capital:</strong> Kabul</p>
+      </div>
+    </div>
+
+    <div class="card w-72 bg-base-100 shadow-xl rounded-md">
+      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <div class="card-body">
+        <h2 class="card-title">Bahamia</h2>
+        <p><strong>Population:</strong> 27657145</p>
+        <p><strong>Region:</strong> Asia</p>
+        <p><strong>Capital:</strong> Kabul</p>
+      </div>
+    </div>
+
     
+
+
+
   </section>
 </section>
 
---
+++ src/routes/countries/[id].svelte
@@ -0,0 +1,19 @@
+<!-- <script>
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+
+  let country = {};
+  let id;
+
+  $: id = $page.params.id;
+
+  onMount(async () => {
+    const response = await fetch('https://restcountries.com/v3.1/name/${id}')
+    if (response.ok) {
+      country = await response.json()
+    }
+  })
+</script>
+
+<h1>{country.name}</h1>
+<h1>{country.symbol}</h1> -->
