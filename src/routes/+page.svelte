2a00f9c0cbf96859ebc309a614ff6d1040c25809
Azdgari <109515968+Azdgari@users.noreply.github.com>
2024-01-17 19:44:25 +0700

    Add country filter function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
 <script>
 
+
   import data from '../data/data.json'
+
+  let selectedRegion = '';
+  let regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+
+  /**
+ * @param {string} selectedRegion
+ */
+  const filteredCountries = (selectedRegion) => {
+    if (selectedRegion === '') {
+      return data;
+    } else {
+      return data.filter((item) => item.region === selectedRegion)
+    }
+    }
+  
 
 </script>
 
@@ -13,24 +29,25 @@
 <section class="mx-14">
   <div class="filter justify-between my-10 md:flex gap-10">
     <input class="flex-2 min-w-96 px-8 h-12 bg-dark-mode-elements rounded-sm" type="text" placeholder="Search for a country..." />
-    <select class="dropdown select mt-8 md:mt-0 select-bordered w-72 max-w-xs bg-dark-mode-elements rounded-sm" >
+    <select bind:value={selectedRegion} class="dropdown select mt-8 md:mt-0 select-bordered w-72 max-w-xs bg-dark-mode-elements rounded-sm" >
       <option value="" disabled selected>Filter by Region</option>
-      <option value="Africa">Africa</option>
-      <option value="America">America</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
+      {#each regions as region}
+      <option>{region}</option>
+      {/each}
+      
     </select>
+
+    <p>Selected region: {selectedRegion}</p>
 
   </div>
     <section class="md:flex flex-wrap gap-20">
 
       {#each data as item (item.name)}
     <div class="card w-72 bg-base-100 shadow-xl rounded-md">
-      <figure><img src="https://flagcdn.com/bs.svg" alt="flag"></figure>
+      <figure><img class="h-44" src="{item.flag}" alt="flag"></figure>
       <div class="card-body">
         <h2 class="card-title">{item.name}</h2>
-        <p><strong>Population:</strong> {item.population}</p>
+        <p><strong>Population:</strong> {item.population.toLocaleString("en-US")}</p>
         <p><strong>Region:</strong> {item.region}</p>
         <p><strong>Capital:</strong> {item.capital}</p>
       </div>
